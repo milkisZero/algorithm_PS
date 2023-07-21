37e3dc8ef9d3e109052f79ef499c2f22acd95742
milkisZero <127817037+milkisZero@users.noreply.github.com>
2023-07-21 11:07:34 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int arr[305];
+int dp[305][2];
+int preStep[305][2];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+
+    dp[1][0] = arr[1];
+    dp[1][1] = arr[1];
+    for (int i = 2; i <= n; i++) {
+        for (int j = 0; j < 2; j++) {
+            int a = dp[i - 1 - j][0] + arr[i];
+            if (j == 0)
+                a = -1;
+
+            int b = dp[i - 1 - j][1] + arr[i];
+            dp[i][j] = max(a, b);
+        }
+    }
+
+    cout << max(dp[n][0], dp[n][1]);
+
+    return 0;
+}
