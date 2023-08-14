cc56810917226f7c787dc8561425e03e60c5e516
milkisZero <127817037+milkisZero@users.noreply.github.com>
2023-08-14 20:57:13 +0900

    [Gold V] Title: 럭키 세븐, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/28706. 럭키 세븐/럭키 세븐.cc
+++ 백준/Gold/28706. 럭키 세븐/럭키 세븐.cc
@@ -1,8 +1,5 @@
 #include <iostream>
-#include <set>
 using namespace std;
-
-set<int> dp[200006];
 
 int main() {
     ios::sync_with_stdio(0);
@@ -14,38 +11,42 @@ int main() {
         int n;
         cin >> n;
 
-        dp[0].insert(1);
+        int dp[7] = {0};
+        dp[1] = 1;
         for (int i = 1; i <= n; i++) {
             char op1, op2;
             int num1, num2;
             cin >> op1 >> num1;
             cin >> op2 >> num2;
 
-            for (auto j = dp[i - 1].begin(); j != dp[i - 1].end(); j++) {
+            int arr[7] = {0};
+            for (int j = 0; j < 7; j++) {
+                if (dp[j] == 0)
+                    continue;
+
                 int tmp;
                 if (op1 == '+')
-                    tmp = *j + num1;
+                    tmp = j + num1;
                 else
-                    tmp = *j * num1;
-                dp[i].insert(tmp % 7);
+                    tmp = j * num1;
+                arr[tmp % 7] = 1;
 
                 if (op2 == '+')
-                    tmp = *j + num2;
+                    tmp = j + num2;
                 else
-                    tmp = *j * num2;
-                dp[i].insert(tmp % 7);
+                    tmp = j * num2;
+                arr[tmp % 7] = 1;
+            }
+
+            for (int j = 0; j < 7; j++) {
+                dp[j] = arr[j];
             }
         }
 
-        auto iter = dp[n].begin();
-        if (*iter == 0)
+        if (dp[0])
             cout << "LUCKY" << '\n';
         else
             cout << "UNLUCKY" << '\n';
-
-        for (int i = 0; i <= n; i++) {
-            dp[i].clear();
-        }
     }
 
     return 0;
