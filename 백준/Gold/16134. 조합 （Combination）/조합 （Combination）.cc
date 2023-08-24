1e3f18a6263d16752974eb4ec36c57ee309fd469
milkisZero <127817037+milkisZero@users.noreply.github.com>
2023-08-24 12:15:24 +0900

    [Gold I] Title: 조합 (Combination), Time: 8 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Gold/16134. 조합 （Combination）/조합 （Combination）.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+using ll = long long;
+#define MOD 1000000007
+
+ll fact[1000004];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL), cout.tie(NULL);
+
+    ll n, r;
+    cin >> n >> r;
+
+    fact[0] = 1;
+    for (ll i = 1; i <= n; i++) {
+        fact[i] = (fact[i - 1] * i) % MOD;
+    }
+
+    ll res = fact[n], inFact = (fact[r] * fact[n - r]) % MOD;
+    ll powN = MOD - 2;
+    while (powN) {
+        if (powN % 2)
+            res = (res * inFact) % MOD;
+        inFact = (inFact * inFact) % MOD;
+        powN /= 2;
+    }
+
+    cout << res;
+
+    return 0;
+}
