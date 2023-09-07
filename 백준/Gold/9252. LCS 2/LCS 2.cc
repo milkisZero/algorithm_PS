3a2334baf7bc3dd6fa82a6c784b2f59841a162e6
milkisZero <127817037+milkisZero@users.noreply.github.com>
2023-09-07 11:15:47 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 5968 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.cc
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -1,12 +1,11 @@
+// #dp #lcs
+// 배열을 역추적하는 방식
 #include <iostream>
 #include <string>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
 int dp[1005][1005];
-pair<int, int> idx[1005][1005];
-char res[1005][1005];
 
 int main() {
     ios::sync_with_stdio(0);
@@ -19,40 +18,29 @@ int main() {
     int b_leng = b.length();
     for (int i = 0; i < a_leng; i++) {
         for (int j = 0; j < b_leng; j++) {
-            if (a[i] == b[j]) {
+            if (a[i] == b[j])
                 dp[i + 1][j + 1] = dp[i][j] + 1;
-                res[i + 1][j + 1] = a[i];
-                idx[i + 1][j + 1] = {i, j};
-            }
-            else {
-                if (dp[i][j + 1] > dp[i + 1][j]) {
-                    dp[i + 1][j + 1] = dp[i][j + 1];
-                    idx[i + 1][j + 1] = idx[i][j + 1];
-                    res[i + 1][j + 1] = res[i][j + 1];
-                }
-                else {
-                    dp[i + 1][j + 1] = dp[i + 1][j];
-                    idx[i + 1][j + 1] = idx[i + 1][j];
-                    res[i + 1][j + 1] = res[i + 1][j];
-                }
-            }
+            else
+                dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]);
+        }
+    }
+    cout << dp[a_leng][b_leng] << '\n';
+
+    string res;
+    int i = a_leng, j = b_leng;
+    while (dp[i][j] != 0) {
+        if (dp[i][j] == dp[i][j - 1])
+            j--;
+        else if (dp[i][j] == dp[i - 1][j])
+            i--;
+        else if (dp[i][j] == dp[i - 1][j - 1] + 1) {
+            res = res + a[i - 1];
+            i--, j--;
         }
     }
 
-    cout << dp[a_leng][b_leng] << '\n';
-    if (!dp[a_leng][b_leng])
-        return 0;
-
-    string tmp;
-    tmp = tmp + res[a_leng][b_leng];
-    pair<int, int> next = idx[a_leng][b_leng];
-
-    for (int i = 1; i < dp[a_leng][b_leng]; i++) {
-        tmp = tmp + res[next.first][next.second];
-        next = idx[next.first][next.second];
-    }
-    reverse(tmp.begin(), tmp.end());
-    cout << tmp;
+    reverse(res.begin(), res.end());
+    cout << res;
 
     return 0;
 }
