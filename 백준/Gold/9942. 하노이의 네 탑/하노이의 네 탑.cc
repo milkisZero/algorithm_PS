0578340a72613994685d1f5cc0152837ae5db3c4
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-02-05 16:55:16 +0900

    [Gold II] Title: 하노이의 네 탑, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/9942. 하노이의 네 탑/하노이의 네 탑.cc
+++ 백준/Gold/9942. 하노이의 네 탑/하노이의 네 탑.cc
@@ -11,36 +11,23 @@ using ll = long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 
-ll dp[1003][5];
-
 ll T(ll n, ll r) {
     if (n == 1)
         return 1;
-    if (r == 3) {
-        if (!dp[n - 1][3])
-            dp[n - 1][3] = T(n - 1, 3);
-        return dp[n][3] = dp[n - 1][3] * 2 + 1;
-    }
+    if (r == 3)
+        return T(n - 1, 3) * 2 + 1;
 
     ll k = n + 1 - round(sqrt(2 * n + 1));
-    if (!dp[k][4])
-        dp[k][4] = T(k, 4);
-    if (!dp[n - k][3])
-        dp[n - k][3] = T(n - k, 3);
-
-    return dp[k][4] * 2 + dp[n - k][3];
+    return  2 * T(k, r) + T(n - k, r - 1);
 }
 
 int main() {
     fastio();
 
-    dp[1][4] = dp[1][3] = 1;
     string s;
-    for (ll i = 1; getline(cin, s); i++) {
+    for (int i = 1; getline(cin, s); i++) {
         ll n = stoi(s);
-        if (!dp[n][4])
-            dp[n][4] = T(n, 4);
-        cout << "Case " << i << ": " << dp[n][4] << '\n';
+        cout << "Case " << i << ": " << T(n, 4) << '\n';
     }
 
     return 0;
