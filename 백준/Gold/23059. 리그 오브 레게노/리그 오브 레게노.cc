de8900fb20bb9b3f443b161c227129b9ae09721f
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-02-08 15:52:52 +0900

    [Gold II] Title: 리그 오브 레게노, Time: 1920 ms, Memory: 112984 KB -BaekjoonHub

---
+++ 백준/Gold/23059. 리그 오브 레게노/리그 오브 레게노.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <map>
+#define INF 0x7fffffff
+#define LLINF 0x7f7f7f7f7f7f
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+
+map<string, vector<string>> m;
+map<string, int> ind;
+vector<string> res;
+vector<string> v;
+priority_queue<string, vector<string>, greater<>> q;
+
+void topo() {
+    for (auto iter = ind.begin(); iter != ind.end(); iter++) {
+        if (iter->second == 0)
+            q.push(iter->first);
+    }
+
+    while (!q.empty()) {
+        string cur = q.top();
+        q.pop();
+        for (auto e : m[cur]) {
+            if (--ind[e] == 0)
+                v.push_back(e);
+        }
+
+        if (q.empty()) {
+            for (auto e : v) {
+                q.push(e);
+            }
+            v.clear();
+        }
+
+        res.push_back(cur);
+    }
+}
+
+int main() {
+    fastio;
+
+    int n;
+    cin >> n;
+
+    while (n--) {
+        string a, b;
+        cin >> a >> b;
+        m[a].push_back(b);
+        if (ind.find(a) == ind.end())
+            ind.insert({a, 0});
+        if (ind.find(b) == ind.end())
+            ind.insert({b, 0});
+        ind[b]++;
+    }
+
+    topo();
+
+    if (res.size() != ind.size())
+        cout << -1;
+    else {
+        for (auto e : res) {
+            cout << e << '\n';
+        }
+    }
+
+    return 0;
+}
