98826a3d1ffb38777b820a0561b5595073007852
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-03-08 10:22:41 +0900

    [Gold III] Title: A[j]-A[i]+A[l]-A[k], Time: 100 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15487. A［j］－A［i］＋A［l］－A［k］/A［j］－A［i］＋A［l］－A［k］.cc
+++ 백준/Gold/15487. A［j］－A［i］＋A［l］－A［k］/A［j］－A［i］＋A［l］－A［k］.cc
@@ -11,9 +11,7 @@ using ll = long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 
-ll arr[1000006];
-ll a1[1000006], a2[1000006];
-ll a3[1000006], a4[1000006];
+ll dp[4] = {-INF, -INF, -INF, -INF};
 
 int main() {
     fastio;
@@ -21,35 +19,17 @@ int main() {
     int n;
     cin >> n;
 
-    for (int i = 1; i <= n; i++) {
-        cin >> arr[i];
+    for (int i = 0; i < n; i++) {
+        ll tmp;
+        cin >> tmp;
+
+        dp[3] = max(dp[3], dp[2] + tmp);
+        dp[2] = max(dp[2], dp[1] - tmp);
+        dp[1] = max(dp[1], dp[0] + tmp);
+        dp[0] = max(dp[0], -tmp);
     }
 
-    a2[0] = a4[n + 1] = INF;
-    for (int i = 1; i <= n; i++) {
-        if (a2[i - 1] > arr[i]) {
-            a1[i] = a2[i] = arr[i];
-        }
-        else {
-            a2[i] = a2[i - 1];
-            a1[i] = max(arr[i], a1[i - 1]);
-        }
-
-        if (a3[n + 1 - i + 1] < arr[n + 1 - i]) {
-            a3[n + 1 - i] = a4[n + 1 - i] = arr[n + 1 - i];
-        }
-        else {
-            a3[n + 1 - i] = a3[n + 1 - i + 1];
-            a4[n + 1 - i] = min(arr[n + 1 - i], a4[n + 1 - i + 1]);
-        }
-    }
-
-    ll res = -INF;
-    for (int i = 1; i <= n - 2; i++) {
-        res = max(res, a1[i] - a2[i - 1] + a3[i + 2] - a4[i + 1]);
-    }
-
-    cout << res;
+    cout << dp[3];
 
     return 0;
 }
