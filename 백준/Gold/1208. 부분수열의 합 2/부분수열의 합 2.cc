7cc454498e9b7d45447291d31795de17c7c77cac
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-03-22 17:15:10 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 868 ms, Memory: 57860 KB -BaekjoonHub

---
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL);
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+ll arr[45];
+map<ll, ll> sum;
+ll n, k, res;
+
+void left_recur(ll idx, ll val) {
+    if (idx == n / 2) {
+        sum[val]++;
+        return;
+    }
+    left_recur(idx + 1, val + arr[idx]);
+    left_recur(idx + 1, val);
+}
+
+void right_recur(ll idx, ll val) {
+    if (idx == n) {
+        res += sum[k - val];
+        return;
+    }
+    right_recur(idx + 1, val + arr[idx]);
+    right_recur(idx + 1, val);
+}
+
+int main() {
+    fastio;
+
+    cin >> n >> k;
+    k == 0 ? res-- : 1;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    left_recur(0, 0);
+    right_recur(n / 2, 0);
+
+    cout << res;
+
+    return 0;
+}
