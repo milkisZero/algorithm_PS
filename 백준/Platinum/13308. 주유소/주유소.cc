09871574471e59fce44a27b294a5b49603be0819
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-03-28 19:51:22 +0900

    [Platinum V] Title: 주유소, Time: 1664 ms, Memory: 201484 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/13308. 주유소/주유소.cc
+++ 백준/Platinum/13308. 주유소/주유소.cc
@@ -8,8 +8,11 @@ using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 using ull = unsigned long long;
 
-ll price[2505], d[4005][2505];
+ll price[2505], d[4005][4005];
 vector<pll> e[2505];
+
+// 다익 -> 다시 왓던 곳을 거쳐 갈 수 있다 -> 상태공간을 하나 늘려?
+// prev -> 현재에는 큰 값이 나중에 작아지는데 먼저 방문한 작은 경로에 의해 갱신이 안됨
 
 int main() {
     fastio;
@@ -29,29 +32,30 @@ int main() {
 
     fill(&d[0][0], &d[n + 2][n + 2], LLINF);
     d[1][0] = 0;
-    priority_queue<pair<pll, pll>> pq;
-    pq.push({{0, INF}, {1, 0}});
+    priority_queue<pair<pll, ll>> pq;
+    pq.push({{0, 0}, 1});
 
+    ll res = LLINF;
     while (!pq.empty()) {
         auto [val, cost] = pq.top().first;
         val = -val;
-        auto [x, cnt] = pq.top().second;
+        ll x = pq.top().second;
         pq.pop();
 
-        if (d[x][cnt] == val) {
+        if (d[x][cost] == val) {
             for (auto [u, v] : e[x]) {
                 ll tmp = min(cost, price[x]);
-                if (val + v * tmp < d[u][cnt + 1] && cnt < n) {
-                    d[u][cnt + 1] = val + v * tmp;
-                    pq.push({{-d[u][cnt + 1], tmp}, {u, cnt + 1}});
+                if (!cost)
+                    tmp = price[x];
+                if (val + v * tmp < d[u][tmp]) {
+                    d[u][tmp] = val + v * tmp;
+                    pq.push({{-d[u][tmp], tmp}, u});
+
+                    if (u == n)
+                        res = min(res, d[n][tmp]);
                 }
             }
         }
-    }
-
-    ll res = LLINF;
-    for (int i = 1; i <= n; i++) {
-        res = min(res, d[n][i]);
     }
 
     cout << res;
