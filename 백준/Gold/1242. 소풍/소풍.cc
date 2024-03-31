2f3a06e2189d609dad3edca337cee647314e7457
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-03-31 21:31:18 +0900

    [Gold I] Title: 소풍, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1242. 소풍/소풍.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+int main() {
+    fastio;
+
+    ll n, k, m;
+    cin >> n >> k >> m;
+
+    ll res = 0;
+    while (n) {
+        res++;
+        m -= k;
+        if (m < 0) {
+            if (n > k) {
+                m = n + m;
+            }
+            else {
+                m = (n - (k % n) + m + k) % n;
+            }
+        }
+        if (m == 0)
+            break;
+        n--;
+    }
+
+    cout << res;
+
+    return 0;
+}
