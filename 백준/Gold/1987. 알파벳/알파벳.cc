3df086caa0f625dbf5faac7e8216a48e97018291
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-04-04 13:49:09 +0900

    [Gold IV] Title: 알파벳, Time: 20 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -11,6 +11,7 @@ using ull = unsigned long long;
 string s[25];
 ll r, c, res;
 vector<pll> add = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+int visited[25][25];
 
 bool inmap(ll a, ll b) {
     return (a < r && b < c && a >= 0 && b >= 0);
@@ -19,6 +20,8 @@ bool inmap(ll a, ll b) {
 void dfs(ll a, ll b, ll cnt, int bit) {
     res = max(res, cnt);
     bit |= 1 << s[a][b] - 'A';
+    if (visited[a][b] == bit) return;
+    visited[a][b] = bit;
     if (bit == (1 << 27) - 1) return;
     for (auto [x1, x2] : add) {
         if (inmap(a + x1, b + x2) && !(bit & (1 << s[a + x1][b + x2] - 'A'))) {
