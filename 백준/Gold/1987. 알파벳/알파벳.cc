052205fe7a4d0c002c11c201ee1026eeffff95ac
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-04-04 13:37:42 +0900

    [Gold IV] Title: 알파벳, Time: 456 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -12,21 +12,18 @@ string s[25];
 ll r, c, res;
 ll visited[25][25];
 vector<pll> add = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-map<char, ll> mp;
 
 bool inmap(ll a, ll b) {
     return (a < r && b < c && a >= 0 && b >= 0);
 }
 
-void dfs(ll a, ll b, ll cnt) {
+void dfs(ll a, ll b, ll cnt, int bit) {
     res = max(res, cnt);
     visited[a][b] = 1;
-    char cur = s[a][b];
-    mp[cur] = 1;
+    bit |= 1 << s[a][b] - 'A';
     for (auto [x1, x2] : add) {
-        if (inmap(a + x1, b + x2) && mp[s[a + x1][b + x2]] == 0 && visited[a + x1][b + x2] == 0) {
-            dfs(a + x1, b + x2, cnt + 1);
-            mp[s[a + x1][b + x2]] = 0;
+        if (inmap(a + x1, b + x2) && !(bit & (1 << s[a + x1][b + x2] - 'A')) && !visited[a + x1][b + x2]) {
+            dfs(a + x1, b + x2, cnt + 1, bit);
             visited[a + x1][b + x2] = 0;
         }
     }
@@ -35,12 +32,8 @@ void dfs(ll a, ll b, ll cnt) {
 int main() {
     fastio;
     cin >> r >> c;
-    for (int i = 0; i < r; i++) {
-        cin >> s[i];
-    }
-
-    dfs(0, 0, 1);
+    for (int i = 0; i < r; i++) cin >> s[i];
+    dfs(0, 0, 1, 0);
     cout << res;
-
     return 0;
 }
