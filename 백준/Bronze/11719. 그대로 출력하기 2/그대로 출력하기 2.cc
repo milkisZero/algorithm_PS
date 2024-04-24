5aac8dd90581174e9983bf5117776cfb6a3f004f
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-04-24 09:04:01 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+int main() {
+    fastio;
+
+    string s;
+    while (getline(cin, s)) {
+        cout << s << '\n';
+    }
+
+    return 0;
+}
