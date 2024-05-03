b20e3533b07eabd661a26073421f2f6f9c0ef321
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-05-03 23:54:14 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+using namespace std;
+int main(){
+    fastio;
+    string s; cin >> s;
+    cout << s.length();
+    return 0;
+}
