ef0e0b6ff59cb8f51b2741d53b1246484283445b
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-05-27 15:57:27 +0900

    [Gold II] Title: 나는 북극곰입니다, Time: 76 ms, Memory: 16032 KB -BaekjoonHub

--- 백준/Gold/31932. 나는 북극곰입니다/나는 북극곰입니다.cc
+++ 백준/Gold/31932. 나는 북극곰입니다/나는 북극곰입니다.cc
@@ -9,35 +9,6 @@ vector<pair<ll, pll>> e[100006];
 ll d[100006];
 ll n, m;
 
-ll calc(ll time) {
-
-    fill(d, d + n + 3, LLINF);
-
-    priority_queue<pll> pq;
-    pq.push({-time, 1});
-    d[1] = time;
-
-    while (!pq.empty()) {
-        auto [val, x] = pq.top();
-        pq.pop();
-        val = -val;
-
-        if (d[x] != val) continue;
-
-        for (auto [v, info] : e[x]) {
-            auto [ddd, t] = info;
-            if (d[v] > val + ddd && val + ddd <= t) {
-                d[v] = val + ddd;
-                pq.push({-d[v], v});
-
-                if (n == v) return 1;
-            }
-        }
-    }
-
-    return 0;
-}
-
 int main() {
     fastio;
 
@@ -50,18 +21,32 @@ int main() {
         e[v].push_back({u, {d, t}});
     }
 
-    ll res = -1;
-    ll l = 0, r = (ll)1e9, mid;
-    while (l <= r) {
-        mid = (l + r) / 2;
-        if (calc(mid)) {
-            res = max(res, mid);
-            l = mid + 1;
+    fill(d, d + n + 3, -1);
+
+    priority_queue<pll> pq;
+    pq.push({0, n});
+    d[n] = 0;
+
+    while (!pq.empty()) {
+        auto [val, x] = pq.top();
+        pq.pop();
+
+        if (d[x] != val) continue;
+
+        for (auto [v, info] : e[x]) {
+            auto [ddd, t] = info;
+
+            ll k = (x == n) ? t : val;
+            k = min(k, t);
+
+            if (d[v] < k - ddd && k - ddd >= 0) {
+                d[v] = k - ddd;
+                if (v != 1) pq.push({d[v], v});
+            }
         }
-        else r = mid - 1;
     }
 
-    cout << res;
+    cout << d[1];
 
     return 0;
 }
