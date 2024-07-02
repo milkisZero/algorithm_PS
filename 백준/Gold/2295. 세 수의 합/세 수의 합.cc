a9b2229e08433819205ef5604e9ada73b0b752e7
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-07-02 14:06:14 +0900

    [Gold IV] Title: 세 수의 합, Time: 116 ms, Memory: 14516 KB -BaekjoonHub

---
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+vector<ll> v, v2;
+map<ll, bool> mp;
+
+bool bs(ll k) {
+    ll l = 0, r = v2.size() - 1, m;
+    while (l <= r) {
+        m = (l + r) / 2;
+
+        if (k == v2[m]) return 1;
+
+        if (k > v2[m]) l = m + 1;
+        else r = m - 1;
+    }
+
+    return 0;
+}
+
+int main() {
+    fastio;
+
+    ll n;
+    cin >> n;
+    v.resize(n);
+    for (auto &e : v) {
+        cin >> e;
+        mp[e] = 1;
+    }
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            v2.push_back(v[i] + v[j]);
+        }
+    }
+    sort(v2.begin(), v2.end());
+    v2.erase(unique(v2.begin(), v2.end()), v2.end());
+
+    ll res = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            ll k = v[i] - v[j];
+
+            if (bs(k)) res = max(res, max(v[i], v[j]));
+        }
+    }
+
+    cout << res;
+
+    return 0;
+}
