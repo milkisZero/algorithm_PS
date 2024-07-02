deb0b3b1cbfb4b6465ce393dcf07737d79f9ad68
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-07-02 14:31:49 +0900

    [Gold IV] Title: 세 수의 합, Time: 52 ms, Memory: 8296 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.cc
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -8,10 +8,10 @@ using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 using ull = unsigned long long;
 
-vector<ll> v, v2;
+vector<int> v2;
 
-bool bs(ll k) {
-    ll l = 0, r = v2.size() - 1, m;
+bool bs(int k) {
+    int l = 0, r = v2.size() - 1, m;
     while (l <= r) {
         m = (l + r) / 2;
 
@@ -27,11 +27,12 @@ bool bs(ll k) {
 int main() {
     fastio;
 
-    ll n;
+    int n;
     cin >> n;
-    v.resize(n);
+
+    int v[n];
     for (auto &e : v) cin >> e;
-    sort(v.begin(), v.end());
+    sort(v, v + n);
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -39,22 +40,17 @@ int main() {
         }
     }
     sort(v2.begin(), v2.end());
-    v2.erase(unique(v2.begin(), v2.end()), v2.end());
 
-    ll res = 0;
     for (int i = n - 1; i >= 0; i--) {
         for (int j = 0; j < n; j++) {
             ll k = v[i] - v[j];
 
             if (bs(k)) {
-                res = v[i];
-                break;
+                cout << v[i];
+                return 0;
             }
         }
-        if (res) break;
     }
-
-    cout << res;
 
     return 0;
 }
