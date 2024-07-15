209ebc35f6a4bdc759571181df2997a56cdf3d67
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-07-15 09:57:35 +0900

    [Bronze IV] Title: 체육은 수학과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32025. 체육은 수학과목 입니다/체육은 수학과목 입니다.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+int main() {
+    fastio;
+
+    double a, b;
+    cin >> a >> b;
+
+    a = min(a, b);
+    cout << (ll)(a / 2 * 100);
+
+    return 0;
+}
