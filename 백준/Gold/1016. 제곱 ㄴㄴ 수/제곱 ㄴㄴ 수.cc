35fea8ec770517a5e7b599585e707ef7f69efa10
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-08-05 11:15:04 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 608 ms, Memory: 76352 KB -BaekjoonHub

---
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+int main() {
+    fastio;
+
+    ll a, b;
+    cin >> a >> b;
+    if (a > b) swap(a, b);
+
+    map<ll, bool> mp;
+    for (ll i = 2; i <= sqrt(b); i++) {
+        for (ll j = a / (i * i); j * i * i <= b; j++) {
+            mp[j * i * i] = 1;
+        }
+    }
+
+    ll cnt = 0;
+    for (ll i = a; i <= b; i++) {
+        if (mp.find(i) == mp.end()) cnt++;
+    }
+
+    cout << cnt;
+
+    return 0;
+}
