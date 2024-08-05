3c77d355983a5da4cba987ac38e34bcf0d2734fa
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-08-05 15:50:26 +0900

    [Platinum V] Title: 책 페이지, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Platinum/1019. 책 페이지/책 페이지.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int cnt[10];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int k = n;
+    for (int j = 0; k; j++) {
+        int tmp = k % 10;
+        k /= 10;
+
+        for (int i = 0; i < 10; i++) {
+            if (i == tmp) {
+                cnt[i] += n - (round(pow(10, j + 1)) * k + tmp * round(pow(10, j))) + 1;
+                if (tmp == 0)
+                    cnt[i] += round(pow(10, j)) * (k - 1);
+                else
+                    cnt[i] += round(pow(10, j)) * k;
+            }
+            else if (tmp < i || i == 0) {
+                cnt[i] += round(pow(10, j)) * k;
+            }
+            else if (i < tmp) {
+                cnt[i] += round(pow(10, j)) * (k + 1);
+            }
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        cout << cnt[i] << ' ';
+    }
+
+    return 0;
+}
