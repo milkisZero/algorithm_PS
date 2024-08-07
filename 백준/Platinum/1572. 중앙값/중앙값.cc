d63ae227995a9722a4edae8d9dabffa9f85fc42d
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-08-07 13:16:14 +0900

    [Platinum V] Title: 중앙값, Time: 144 ms, Memory: 35228 KB -BaekjoonHub

--- 백준/Platinum/1572. 중앙값/중앙값.cc
+++ 백준/Platinum/1572. 중앙값/중앙값.cc
@@ -22,11 +22,11 @@ void upd(ll n, ll l, ll r, ll t, ll val) {
     sg[n] = sg[2 * n] + sg[2 * n + 1];
 }
 
-ll qry(ll n, ll l, ll r, ll ql, ll qr) {
-    if (qr < l || r < ql) return 0;
-    else if (ql <= l && r <= qr) return sg[n];
+ll find_value(ll n, ll l, ll r, ll val) {
+    if (l == r) return l;
     ll m = (l + r) / 2;
-    return qry(2 * n, l, m, ql, qr) + qry(2 * n + 1, m + 1, r, ql, qr);
+    if (sg[2 * n] < val) return find_value(2 * n + 1, m + 1, r, val - sg[2 * n]);
+    else return find_value(2 * n, l, m, val);
 }
 
 int main() {
@@ -40,28 +40,14 @@ int main() {
     vector<ll> vec(n + 1);
     vec[0] = 0;
     for (int i = 1; i <= n; i++) {
-        ll tmp;
-        cin >> tmp;
-        tmp++;
+        cin >> vec[i];
+        vec[i]++;
 
-        vec[i] = tmp;
-
-        upd(1, 1, MAX, tmp, 1);
+        upd(1, 1, MAX, vec[i], 1);
 
         if (i >= k) {
             upd(1, 1, MAX, vec[i - k], -1);
-
-            ll l = 1, r = MAX, m;
-            while (l < r) {
-                m = (l + r) / 2;
-
-                ll v = qry(1, 1, MAX, 1, m);
-
-                if (v < (k + 1) / 2) l = m + 1;
-                else r = m;
-            }
-
-            sum += r - 1;
+            sum += find_value(1, 1, MAX, (k + 1) / 2) - 1;
         }
     }
 
