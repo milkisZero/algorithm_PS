8e1f860c0cd2c66a48d6d506cb34d26f33a1204c
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-08-19 13:43:17 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 176 ms, Memory: 25468 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -1,47 +1,39 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
 using namespace std;
-
-int arr[1000006];
-int lis[1000006];
-int index[1000006];
+using ll = long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(NULL), cout.tie(NULL);
+    fastio;
 
-    int n;
+    ll n;
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
 
-    int j = 0;
-    lis[0] = arr[0];
-    for (int i = 0; i < n; i++) {
-        if (lis[j] >= arr[i]) {
-            int idx = lower_bound(lis, lis + j, arr[i]) - lis;
-            lis[idx] = arr[i];
-            index[i] = idx;
-        }
+    vector<ll> v(n), lis(n), index(n);
+    for (auto &e : v) cin >> e;
+
+    ll idx = 0;
+    lis[0] = v[0], index[0] = 0;
+    for (int i = 1; i < n; i++) {
+        if (lis[idx] < v[i]) lis[++idx] = v[i], index[i] = idx;
         else {
-            lis[++j] = arr[i];
-            index[i] = j;
+            ll idx2 = lower_bound(lis.begin(), lis.begin() + idx, v[i]) - lis.begin();
+            lis[idx2] = v[i], index[i] = idx2;
         }
     }
 
-    int tmp = j;
+    ll tmp = idx;
     for (int i = n - 1; i >= 0; i--) {
-        if (tmp == index[i]) {
-            lis[index[i]] = arr[i];
-            tmp--;
-        }
+        if (tmp == index[i]) lis[index[i]] = v[i], tmp--;
     }
 
-    cout << j + 1 << '\n';
-    for (int i = 0; i <= j; i++) {
-        cout << lis[i] << ' ';
-    }
+    cout << idx + 1 << '\n';
+    for (int i = 0; i <= idx; i++) cout << lis[i] << ' ';
 
     return 0;
 }
