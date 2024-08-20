77b49b3da2c7b5f0a98ca3df4e568ea9891fb033
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-08-20 14:32:40 +0900

    [Bronze V] Title: HI-ARC=?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+int main() {
+    fastio;
+
+    ll a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a * b - c * d * e;
+
+    return 0;
+}
