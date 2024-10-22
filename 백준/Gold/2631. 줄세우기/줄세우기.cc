d851631593210098fdbb043166ce3988f8101650
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-10-22 19:31:49 +0900

    [Gold IV] Title: 줄세우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+#define endl '\n'
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+int main() {
+    fastio;
+
+    ll n;
+    cin >> n;
+
+    vector<ll> v(n);
+    for (auto &e : v) cin >> e;
+
+    ll idx = 0;
+    vector<ll> lis(n);
+    lis[0] = v[0];
+    for (int i = 0; i < n; i++) {
+        if (lis[idx] < v[i]) lis[++idx] = v[i];
+        else {
+            ll idx2 = lower_bound(lis.begin(), lis.begin() + idx, v[i]) - lis.begin();
+            lis[idx2] = v[i];
+        }
+    }
+
+    cout << n - idx - 1;
+
+    return 0;
+}
