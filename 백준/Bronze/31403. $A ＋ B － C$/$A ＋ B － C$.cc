365de00c737cfad3e99663683090e4e3817e8f80
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-11-03 16:03:13 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+#define endl '\n'
+#define sp ' '
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+int main() {
+    fastio;
+
+    ll a, b, c;
+    cin >> a >> b >> c;
+
+    string s = to_string(a) + to_string(b);
+    ll n = stoi(s);
+
+    cout << a + b - c << endl
+         << n - c;
+
+    return 0;
+}
