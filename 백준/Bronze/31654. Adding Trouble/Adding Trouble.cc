dd1f1732f762ad566a8afc69b8a14435e7160975
milkisZero <127817037+milkisZero@users.noreply.github.com>
2024-11-06 00:07:56 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+#define endl '\n'
+#define sp ' '
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+int main() {
+    fastio;
+
+    ll a, b, c;
+    cin >> a >> b >> c;
+    cout << ((a + b == c) ? "correct!" : "wrong!");
+
+    return 0;
+}
