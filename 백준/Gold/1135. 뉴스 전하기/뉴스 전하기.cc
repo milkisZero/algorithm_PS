71891f3776edc01ef15412ccbd30c12a87169702
milkisZero <127817037+milkisZero@users.noreply.github.com>
2025-01-09 00:35:25 +0900

    [Gold II] Title: 뉴스 전하기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+#define endl '\n'
+#define sp ' '
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+vector<ll> v[55];
+ll visited[55];
+ll cnt[55];
+
+ll dfs(ll cur) {
+    visited[cur] = 1;
+
+    ll tmp = 0;
+    for (auto e : v[cur]) {
+        if (!visited[e]) tmp += dfs(e);
+    }
+
+    sort(v[cur].begin(), v[cur].end(), [](ll a, ll b) { return cnt[a] > cnt[b]; });
+
+    ll k = 0, sum = 0;
+    for (auto e : v[cur]) {
+        sum = max(sum, k + cnt[e]);
+        k++;
+    }
+
+    return cnt[cur] = sum + 1;
+}
+
+int main() {
+    fastio;
+
+    ll n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        ll tmp;
+        cin >> tmp;
+        if (tmp > -1) v[tmp].push_back(i);
+    }
+
+    cout << dfs(0) - 1;
+
+    return 0;
+}
