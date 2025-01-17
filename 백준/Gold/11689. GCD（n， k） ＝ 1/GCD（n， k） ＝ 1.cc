7f4ea3a153f1dfdfc7088ba3993bdbfbe0a1cf2e
milkisZero <127817037+milkisZero@users.noreply.github.com>
2025-01-18 02:31:27 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.cc
@@ -0,0 +1,33 @@
+// #수학 #오일러 피함수
+// 공식을 이용, 소인수 구하는 과정 반복
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+#define endl '\n'
+#define sp ' '
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+int main() {
+    fastio;
+
+    ll n;
+    cin >> n;
+
+    ll res = n;
+    for (ll i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            res -= res / i;
+            while (n % i == 0) n /= i;
+        }
+    }
+    if (n > 1) res -= res / n;
+
+    cout << res;
+
+    return 0;
+}
