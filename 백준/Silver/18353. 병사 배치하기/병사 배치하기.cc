ae1e39ddf84748bb4ce260af587221daf5479039
milkisZero <127817037+milkisZero@users.noreply.github.com>
2026-01-18 12:23:28 +0900

    [Silver II] Title: 병사 배치하기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+#define endl '\n'
+#define sp ' '
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+int main() {
+    fastio;
+
+    ll n;
+    cin >> n;
+
+    vector<ll> v(n);
+    for (auto &e : v) cin >> e;
+
+    vector<ll> lis(n);
+    ll idx = 0;
+    lis[0] = v[n - 1];
+    for (int i = n - 1; i >= 0; i--) {
+        if (lis[idx] < v[i]) lis[++idx] = v[i];
+        else {
+            ll idx2 = lower_bound(lis.begin(), lis.begin() + idx, v[i]) - lis.begin();
+            lis[idx2] = v[i];
+        }
+    }
+
+    cout << n - (idx + 1);
+
+    return 0;
+}
